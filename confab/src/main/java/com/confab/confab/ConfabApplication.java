262e0f5bb3d0047faec7ab333614e7be839b7e50
MahendarEnnam <40671888+MahendarEnnam@users.noreply.github.com>
2020-08-15 11:30:35 +0530

    first commit

---
+++ confab/src/main/java/com/confab/confab/ConfabApplication.java
@@ -0,0 +1,13 @@
+package com.confab.confab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfabApplication.class, args);
+	}
+
+}
---
+++ confab/src/test/java/com/confab/confab/ConfabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confab.confab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
